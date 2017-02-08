b11ebbd8bb321b228e0dc8a5191f181f91952ea2
lzh <414676265@qq.com>
2017-02-08 20:39:20 +0800

    添加MD5、网络返回结果的解析工具类和用户注册

---
+++ superwechat/src/cn/ucai/superwechat/net/NetDao.java
@@ -0,0 +1,36 @@
+package cn.ucai.superwechat.net;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.utils.MD5;
+import cn.ucai.superwechat.utils.OkHttpUtils;
+
+/**
+ * Created by Administrator on 2017/2/8.
+ */
+
+public class NetDao {
+    public static void register(Context context, String username, String nick, String password,
+                                OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME, username)
+                .addParam(I.User.NICK, nick)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
+                .targetClass(String.class)
+                .post()
+                .execute(listener);
+
+    }
+    public static void unregister(Context context, String username,
+                                OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME, username)
+                .targetClass(String.class)
+                .execute(listener);
+
+    }
+}
