2d85ee3a48ed84add975b831a5ad38ea00b90653
lzh <414676265@qq.com>
2017-02-15 10:08:31 +0800

    新的朋友界面优化

--- superwechat/src/cn/ucai/superwechat/ui/NewFriendsMsgActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/NewFriendsMsgActivity.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,14 +15,20 @@ package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.adapter.NewFriendsMsgAdapter;
 import cn.ucai.superwechat.db.InviteMessgeDao;
 import cn.ucai.superwechat.domain.InviteMessage;
-
-import java.util.List;
+import cn.ucai.superwechat.utils.MFGT;
 
 /**
  * Application and notification
@@ -30,22 +36,51 @@ import java.util.List;
  */
 public class NewFriendsMsgActivity extends BaseActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.em_activity_new_friends_msg);
+    @BindView(R.id.img_back)
+    ImageView imgBack;
+    @BindView(R.id.tex_title)
+    TextView texTitle;
+    @BindView(R.id.tex_right)
+    TextView texRight;
 
-		ListView listView = (ListView) findViewById(R.id.list);
-		InviteMessgeDao dao = new InviteMessgeDao(this);
-		List<InviteMessage> msgs = dao.getMessagesList();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.em_activity_new_friends_msg);
+        ButterKnife.bind(this);
 
-		NewFriendsMsgAdapter adapter = new NewFriendsMsgAdapter(this, 1, msgs);
-		listView.setAdapter(adapter);
-		dao.saveUnreadMessageCount(0);
-		
-	}
+        ListView listView = (ListView) findViewById(R.id.list);
+        InviteMessgeDao dao = new InviteMessgeDao(this);
+        List<InviteMessage> msgs = dao.getMessagesList();
 
-	public void back(View view) {
-		finish();
-	}
+        NewFriendsMsgAdapter adapter = new NewFriendsMsgAdapter(this, 1, msgs);
+        listView.setAdapter(adapter);
+        dao.saveUnreadMessageCount(0);
+
+        initView();
+
+    }
+
+    private void initView() {
+        imgBack.setVisibility(View.VISIBLE);
+        texTitle.setVisibility(View.VISIBLE);
+        texTitle.setText(R.string.Application_and_notify);
+        texRight.setVisibility(View.VISIBLE);
+    }
+
+    public void back(View view) {
+        finish();
+    }
+
+    @OnClick({R.id.img_back, R.id.tex_right})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.img_back:
+                MFGT.finish(this);
+                break;
+            case R.id.tex_right:
+                MFGT.gotoAddContact(this);
+                break;
+        }
+    }
 }
